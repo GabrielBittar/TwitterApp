63815dd6a3834690e2c204b4b33c3e710a0d1020
gblinux <gabrielfilosofiaprofessor@gmail.com>
2020-09-27 11:02:21 -0400

    Adiciona metodos do twitter4j, remove classe desnecessaria, cria metodo main

--- src/main/java/TwitterApp.java
+++ src/main/java/TwitterApp.java
@@ -4,11 +4,56 @@ import com.mongodb.DBCollection;
 import com.mongodb.Mongo;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoException;
+
 import java.net.UnknownHostException;
+
 import twitter4j.*;
 import twitter4j.conf.ConfigurationBuilder;
 
 public class TwitterApp {
     private ConfigurationBuilder cb;
+    private DB database;
+    private DBCollection collection;
 
+    public void capturaTweets() throws InterruptedException {
+
+    }
+
+    public void configuraCredenciais() {
+        TwitterStream twitterStream = new TwitterStreamFactory(cb.build()).getInstance();
+        StatusListener listener = new StatusListener() {
+            public void onStatus(Status status) {
+                BasicDBObject dbObject = new BasicDBObject();
+
+            }
+
+            public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {
+
+            }
+
+            public void onTrackLimitationNotice(int i) {
+
+            }
+
+            public void onScrubGeo(long l, long l1) {
+
+            }
+
+            public void onStallWarning(StallWarning stallWarning) {
+
+            }
+
+            public void onException(Exception e) {
+
+            }
+        }
+    }
+
+    public void conectaMongoDB() {
+
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+
+    }
 }
