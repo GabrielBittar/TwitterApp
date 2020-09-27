ebf72e7f22d6ad353d5e7fd151007a22b88da5ff
gblinux <gabrielfilosofiaprofessor@gmail.com>
2020-09-27 10:43:27 -0400

    Iniciando projeto, importando libraries

---
+++ src/main/java/TwitterApp.java
@@ -0,0 +1,14 @@
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.Mongo;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoException;
+import java.net.UnknownHostException;
+import twitter4j.*;
+import twitter4j.conf.ConfigurationBuilder;
+
+public class TwitterApp {
+    private ConfigurationBuilder cb;
+
+}
